d7d9697c5851cdf341d422ecc78777515b67165f
Ahmed Naeem <ahmednaeem4553@gmail.com>
2026-02-20 15:38:03 +0500

    20/02/2026

--- lib/res/components/round_button.dart
+++ lib/res/components/round_button.dart
@@ -1,24 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:socianity/res/color.dart';
 
 class RoundButton extends StatelessWidget {
   final String title;
-  const RoundButton({super.key, required this.title});
+  final VoidCallback onPressed;
+  final Color color, textColor;
+  final bool loading;
+  const RoundButton({
+    super.key,
+    required this.title,
+    required this.onPressed,
+    this.textColor = AppColors.primaryTextTextColor,
+    this.color = AppColors.primaryColor,
+    this.loading = false,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 50,
-      decoration: BoxDecoration(
-        color: Colors.red,
-        borderRadius: BorderRadius.circular(30),
-      ),
-      child: Center(
-        child: Text(
-          title,
-          style: Theme.of(
-            context,
-          ).textTheme.headlineMedium!.copyWith(fontSize: 16),
+    return InkWell(
+      onTap: loading ? null : onPressed,
+      child: Container(
+        height: 50,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.circular(30),
         ),
+        child: loading
+            ? Center(child: CircularProgressIndicator(color: Colors.white))
+            : Center(
+                child: Text(
+                  title,
+                  style: Theme.of(
+                    context,
+                  ).textTheme.headlineMedium!.copyWith(fontSize: 16),
+                ),
+              ),
       ),
     );
   }
